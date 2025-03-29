2697e5102cef71e1a629177a56d994e373b76f7a
IlTuoNome <laTuaEmail@example.com>
2025-03-29 13:13:24 +0100

    Add bootstrap-icons and enhance App component with image display

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 const api_endpoint = "http://localhost:3000/api/v1/post";
+const apiImg = "http://localhost:3000/images";
 
 export default function App() {
   const [posts, setPosts] = useState([]);
@@ -21,6 +22,47 @@ export default function App() {
       })
 
   }
+
+
+  return (
+    <>
+
+      <main>
+
+        <div>
+          <div className="container">
+            <h1 className="text-center text-info p-3 ">Blog Di Eduard  <i class="bi bi-box2-heart"></i></h1>
+          </div>
+
+        </div>
+        <div className="container ">
+          <div className="row flex-column ">
+            {
+              posts.map((element, index) =>
+                <div className="col p-3 " key={index}>
+                  <div className="card d-flex flex-row ">
+                    <div className='img-container d-flex'>
+                      <img src={`${apiImg}/${element.image}`} className="card-img-top" alt="..." />
+                    </div>
+                    <div className="card-body">
+                      <div className="div d-flex column-gap-3">
+                        <h5 className="card-title">{element.title}</h5>
+                      </div>
+                      <p className="card-text">{element.content}</p>
+                    </div>
+                  </div>
+                </div>
+
+              )}
+
+          </div>
+        </div>
+
+      </main>
+
+
+    </>
+  )
 
 
 
