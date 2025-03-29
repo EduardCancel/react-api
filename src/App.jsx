eb3899369eca807ee4fa0147cb96c74dd9d38c72
IlTuoNome <laTuaEmail@example.com>
2025-03-29 12:39:36 +0100

    Add CORS dependency and implement data fetching in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
+
+const api_endpoint = "http://localhost:3000/api/v1/post";
+
+export default function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchData(api_endpoint)
+  }, [])
 
 
-function App() {
+  function fetchData() {
+    console.log('you click');
+    fetch(api_endpoint)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
 
-  return (
-    <>
+        setPosts(data)
+      })
 
-    </>
-  )
+  }
+
+
+
 }
-
-export default App
